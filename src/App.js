dd600da87e79112c33e1aaeae3df24563851fde2
ChetanXpro <chetanbaliyan10@gmail.com>
2022-09-16 16:52:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import Api from "./Components/API";
+import LoadingPage from "./Components/LoadingPage";
 
 function App() {
+  const [users, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    const res = await fetch("https://api.github.com/users");
+    setIsLoading(false);
+    const data = await res.json();
+
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isLoading ? <Api users={users} /> : <LoadingPage />}
     </div>
   );
 }
---
+++ src/Components/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default LoadingPage;
